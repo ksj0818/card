a327122d61489d5dde716d49365f060c0f2211ed
ksj0818 <ksj071808@gmail.com>
2021-09-23 14:10:31 +0900

    Create index
    카드 넘버 입력 다음 입력창으로 넘어가기 기능

---
+++ resources/js/index.js
@@ -0,0 +1,20 @@
+const inputElements = document.getElementsByClassName('card-number')
+for (let index = 0; index < inputElements.length; index++) {
+  let inputElement = inputElements[index]
+  inputElement.dataset.index = index
+  inputElement.addEventListener('input', focusElement)
+}
+
+function focusElement(event) {
+  if (event.target.value.length == 4) {      
+    let currentIndex = Number(this.dataset.index)
+    if (inputElements[currentIndex + 1] != undefined) {
+      inputElements[currentIndex + 1].focus()
+    }
+  } else if (event.target.value.length == 0) {
+    let currentIndex = Number(this.dataset.index)
+    if (inputElements[currentIndex - 1] != undefined) {
+      inputElements[currentIndex - 1].focus()
+    }
+  } 
+}
