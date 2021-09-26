d6d494eaaf9b3636344f157eaf6d5a9a675c7538
ksj0818 <ksj071808@gmail.com>
2021-09-26 23:33:53 +0900

    Create Index
    카드번호 css 생성

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,20 +1,6 @@
-const inputElements = document.getElementsByClassName('card-number')
-for (let index = 0; index < inputElements.length; index++) {
-  let inputElement = inputElements[index]
-  inputElement.dataset.index = index
-  inputElement.addEventListener('input', focusElement)
-}
+const cardInputs = document.getElementsByClassName('card-input');
 
-function focusElement(event) {
-  if (event.target.value.length == 4) {      
-    let currentIndex = Number(this.dataset.index)
-    if (inputElements[currentIndex + 1] != undefined) {
-      inputElements[currentIndex + 1].focus()
-    }
-  } else if (event.target.value.length == 0) {
-    let currentIndex = Number(this.dataset.index)
-    if (inputElements[currentIndex - 1] != undefined) {
-      inputElements[currentIndex - 1].focus()
-    }
-  } 
-} 
+for (let i = 0; i < cardInputs.length; i++) {
+  let cardInput = cardInputs[i];
+  console.log(cardInput);
+}
