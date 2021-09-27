c58452ee2b9ae580565d138b4b771941bd4cb761
ksj0818 <ksj071808@gmail.com>
2021-09-27 13:41:29 +0900

    Update index.js
    카드 다음 및 이전 텍스트 이동 기능 추가

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,5 +2,21 @@ const cardInputs = document.getElementsByClassName('card-input');
 
 for (let i = 0; i < cardInputs.length; i++) {
   let cardInput = cardInputs[i];
-  console.log(cardInput);
+  cardInput.dataset.indexNumber = i;
+  cardInput.addEventListener('input', focusInput);
+}
+
+// functions
+function focusInput(event) {
+  let eventTargetValue = event.target.value;
+  let currentIndex = Number(this.dataset.indexNumber);
+  if (eventTargetValue.length === 4) {
+    if (cardInputs[currentIndex + 1] != undefined) {
+      cardInputs[currentIndex + 1].focus();
+    } 
+  } else if (eventTargetValue.length === 0) {
+    if (cardInputs[currentIndex - 1] != undefined) {
+      cardInputs[currentIndex - 1].focus();
+    }
+  }
 }
